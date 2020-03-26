4afbae2d09c513c79b6dba34c3b2463c5872a2ee
Kevin Mattos <kevinmmattos@gmail.com>
2020-03-26 17:28:06 -0300

    commit on Server

---
+++ src/servidor.js
@@ -0,0 +1,40 @@
+var options = {
+    inflate: true,
+    limit: '100kb',
+    type: 'application/octet-stream'
+  }
+
+const porta = 3003
+const express = require("express")
+app = express()
+const banco = require("./bancoDeDados")
+const parser = require("body-parser")
+
+
+app.use(parser.urlencoded())
+app.use(parser.raw(options))
+app.use(parser.json())
+
+app.get('/produtos', (req, res, next) => {
+    res.send(banco.getProdutos())
+})
+app.get('/produto/:id', (req, res, next) => {
+    res.send(banco.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = banco.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+
+
+
+
+app.listen(porta, () =>{
+    console.log(`servidor esta executando na porta ${porta}`)
+})
+console.log("oie")
